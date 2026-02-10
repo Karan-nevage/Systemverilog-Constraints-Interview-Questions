0effe8a7305f07b4a68a0e4674b1e8370bbc34aa
Karankumar Nevage <111281049+Karan-nevage@users.noreply.github.com>
2026-02-10 19:44:39 +0530

    Question 42

---
+++ Easy Coding Question/q42.sv
@@ -0,0 +1,27 @@
+// 42. Write a constraint for a 16‑bit addr which should contain the 9ᵗʰ bit as 1 but should not have consecutive 1s
+
+
+class sample;
+  rand bit[15:0] a ;
+
+  constraint cons_1{
+    a[9] == 1'b1;}
+
+  constraint cons_2{
+    foreach (a[i]) if (i < 15) !(a[i] && a[i+1]);
+  }
+
+endclass
+
+module top;
+  sample s;
+  initial begin
+    s = new();
+    repeat(10) begin
+      assert(s.randomize());
+      $display(" Value of addr: \n A = %b", s.a);
+      $display("===================================");
+    end
+  end  
+endmodule
+
